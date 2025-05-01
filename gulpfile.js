c31c5adc41c84ff6aedcf6439ff9090011db9393
VAIBHAV <40538153+vaibhav1307@users.noreply.github.com>
2025-05-02 03:03:38 +0530

    fix1

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,38 @@
 // gulpfile.js
-const path        = require('path');
 const { src, dest, watch, series, parallel } = require('gulp');
-const sass        = require('gulp-sass')(require('sass'));
-const postcss     = require('gulp-postcss');
-const autoprefixer= require('autoprefixer');
-const concat      = require('gulp-concat');
-const uglify      = require('gulp-uglify');
-const sourcemaps  = require('gulp-sourcemaps');
-const browserSync = require('browser-sync').create();
+const sass         = require('gulp-sass')(require('sass'));
+const postcss      = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const concat       = require('gulp-concat');
+const uglify       = require('gulp-uglify');
+const sourcemaps   = require('gulp-sourcemaps');
+const browserSync  = require('browser-sync').create();
+const fileInclude  = require('gulp-file-include');
 
-// Paths
 const paths = {
-  html:   { src: 'src/*.html',             dest: 'dist/' },
-  styles: { src: 'src/styles/main.scss',   dest: 'dist/styles/' },
-  scripts:{ src: 'src/components/**/*.js', dest: 'dist/' },
-  assets: { src: 'src/assets/**/*',        dest: 'dist/assets/' }
+  html:    { src: 'src/index.html',        dest: 'dist/' },
+  styles:  { src: 'src/styles/main.scss',  dest: 'dist/css/' },
+  scripts: { src: 'src/components/**/*.js',dest: 'dist/js/' },
+  assets:  { src: 'src/assets/**/*',       dest: 'dist/assets/' }
 };
 
-// Compile main.scss → main.css
+// HTML + includes
+function html() {
+  return src(paths.html.src)
+    .pipe(fileInclude({
+      prefix: '@@',
+      basepath: 'src'
+    }))
+    .pipe(dest(paths.html.dest))
+    .pipe(browserSync.stream());
+}
+
+// SCSS → CSS
 function styles() {
   return src(paths.styles.src)
     .pipe(sourcemaps.init())
-    .pipe(sass.sync({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(sass.sync({ outputStyle: 'expanded' })
+      .on('error', sass.logError))
     .pipe(postcss([autoprefixer()]))
     .pipe(concat('main.css'))
     .pipe(sourcemaps.write('.'))
@@ -29,19 +40,12 @@ function styles() {
     .pipe(browserSync.stream());
 }
 
-// Bundle & minify JS → main.js
+// JS → bundle
 function scripts() {
   return src(paths.scripts.src, { sourcemaps: true })
     .pipe(concat('main.js'))
     .pipe(uglify())
     .pipe(dest(paths.scripts.dest, { sourcemaps: '.' }))
-    .pipe(browserSync.stream());
-}
-
-// Copy HTML
-function html() {
-  return src(paths.html.src)
-    .pipe(dest(paths.html.dest))
     .pipe(browserSync.stream());
 }
 
@@ -51,25 +55,16 @@ function assets() {
     .pipe(dest(paths.assets.dest));
 }
 
-// Dev server + watch
+// Watch + serve
 function serve() {
-  browserSync.init({
-    server: { baseDir: 'dist/' }
-  });
+  browserSync.init({ server: 'dist/' });
+  watch('src/**/*.html', html);
   watch('src/styles/**/*.scss', styles);
   watch(paths.scripts.src, scripts);
-  watch(paths.html.src, html);
   watch(paths.assets.src, assets);
 }
 
-// Build task
-const build = series(
-  parallel(styles, scripts, html, assets)
+exports.default = series(
+  parallel(html, styles, scripts, assets),
+  serve
 );
-
-exports.styles  = styles;
-exports.scripts = scripts;
-exports.html    = html;
-exports.assets  = assets;
-exports.build   = build;
-exports.default = series(build, serve);
